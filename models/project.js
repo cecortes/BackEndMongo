bbbeb3376a94545888dc2d67e05c764532285986
César López Cortés <cesarlopezcortes@hotmail.com>
2022-08-06 11:03:20 -0500

    Creación de Modelos para la base de datos

---
+++ models/project.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Import moongose object
+const mongoose = require("mongoose");
+
+// Create a schema
+const Schema = mongoose.Schema;
+
+// Create a project schema
+const ProjectSchema = Schema({
+  // Create properties for the schema
+  name: String,
+  description: String,
+  category: String,
+  year: Number,
+  langs: [String],
+});
+
+// Export the project schema
+module.exports = mongoose.model("Project", ProjectSchema);
